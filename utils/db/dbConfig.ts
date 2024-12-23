10cd90a9c6dec8e4035b5bc2d2cff74c535b9f11
ong22280 <ong22280@gmail.com>
2024-12-23 23:28:06 +0700

    test deploy

--- utils/db/dbConfig.ts
+++ utils/db/dbConfig.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/neon-http"
 import * as schema from "./schema"
 
 // Initialize the Neon database connection
-const sql = neon(process.env.DATABASE || "")
+const sql = neon(process.env.DATABASE || '')
 
 // Initialize Drizzle ORM with the Neon connection and schema
 export const db = drizzle(sql, { schema })
